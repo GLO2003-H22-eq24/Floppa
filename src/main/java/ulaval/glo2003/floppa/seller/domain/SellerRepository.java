e65bf2a644cbd7a242eeb43584d6bd6c9ddb3942
Sébastien Bélanger-Van Coillie <sebastien.belanger-van-coillie.1@ulaval.ca>
2022-03-06 18:10:07 -0500

    retrieve Seller by product: refactor dans le repo le code pour trouver les products

--- src/main/java/ulaval/glo2003/floppa/seller/domain/SellerRepository.java
+++ src/main/java/ulaval/glo2003/floppa/seller/domain/SellerRepository.java
@@ -2,6 +2,7 @@ package ulaval.glo2003.floppa.seller.domain;
 
 
 import ulaval.glo2003.floppa.app.domain.ErrorException;
+import ulaval.glo2003.floppa.product.domain.Product;
 
 import java.util.List;
 import java.util.function.Function;
@@ -11,5 +12,7 @@ public interface SellerRepository {
 
 	Seller retrieveSeller(String sellerId) throws ErrorException;
 
-	List<Seller> retrieveSeller(List<Function<Seller, Boolean>> filterConditions);
+	List<Product> findProductsByConditions(List<Function<Seller, Boolean>> sellerConditions, List<Function<Product, Boolean>> productConditions);
+
+	List<Seller> retrieveSeller(List<Function<Seller, Boolean>> sellerConditions);
 }
