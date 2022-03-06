d789d9061b0c88ab02b13c389bdb0b3e3886e2ea
Sébastien Bélanger-Van Coillie <sebastien.belanger-van-coillie.1@ulaval.ca>
2022-03-06 15:44:13 -0500

    creation_produit_backend; Ajouter le service de produit (use case; creation d'une produit pour un seller)

--- src/main/java/ulaval/glo2003/floppa/product/api/ProductResource.java
+++ src/main/java/ulaval/glo2003/floppa/product/api/ProductResource.java
@@ -5,18 +5,34 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.*;
+import ulaval.glo2003.floppa.app.domain.ErrorCode;
 import ulaval.glo2003.floppa.app.domain.ErrorException;
 import ulaval.glo2003.floppa.product.api.message.ProductCreationDtoRequest;
+import ulaval.glo2003.floppa.product.applicative.ProductService;
+import ulaval.glo2003.floppa.product.domain.Product;
+
+import java.net.URI;
+import java.util.Optional;
 
 @Path("/products")
 public class ProductResource {
+	private ProductService productService;
+	private ProductAssembler productAssembler;
+
+	@Inject
+	public ProductResource(ProductService productService, ProductAssembler productAssembler) {
+		this.productService = productService;
+		this.productAssembler = productAssembler;
+	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createProduct(ProductCreationDtoRequest productCreationDtoRequest, @Context UriInfo uriInfo, @Context HttpHeaders headers) throws ErrorException {
-		//TODO: faire les call au repo
-		return null;
+		String sellerId = getSellerId(headers);
+		Product product = this.productAssembler.fromDto(productCreationDtoRequest);
+		this.productService.createProductForSeller(sellerId, product);
+		return Response.status(Response.Status.CREATED).location(URI.create(uriInfo.getBaseUri() + "products/" + product.getId())).build();
 	}
 
 
