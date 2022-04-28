960f329acc58df25de166828615d46109b59f37c
Sébastien Bélanger-Van Coillie <sebastien.belanger-van-coillie.1@ulaval.ca>
2022-04-27 20:10:25 -0400

    cabages

--- src/main/java/ulaval/glo2003/FloppaRunnable.java
+++ src/main/java/ulaval/glo2003/FloppaRunnable.java
@@ -21,6 +21,7 @@ public class FloppaRunnable implements Runnable {
         AppConfigFactory appConfigFactory = new AppConfigFactory(new ArgConfigResolver(), new SystemConfigResolver(), new DefaultConfigResolver());
         FloppaRunnable floppaRunnable = new FloppaRunnable(appConfigFactory.createAppConfig(args));
         floppaRunnable.run();
+        System.out.println("asd");
     }
 
     @Override
